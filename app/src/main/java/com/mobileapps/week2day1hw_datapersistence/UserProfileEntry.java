8cbb166b13d548da27ed43870792cb119b33fa6a
Adam Noel <adam.noel@mobileappscompany.us>
2018-11-20 14:19:03 -0500

    TODO: Complete Data Entry Screen

--- app/src/main/java/com/mobileapps/week2day1hw_datapersistence/UserProfileEntry.java
+++ app/src/main/java/com/mobileapps/week2day1hw_datapersistence/UserProfileEntry.java
@@ -1,8 +1,11 @@
 package com.mobileapps.week2day1hw_datapersistence;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -10,6 +13,7 @@ import android.widget.TextView;
 import static com.mobileapps.week2day1hw_datapersistence.Constants.DATABASE_VERSION;
 import static com.mobileapps.week2day1hw_datapersistence.Constants.DATABSE_NAME;
 import static com.mobileapps.week2day1hw_datapersistence.Constants.PASSWORD;
+import static com.mobileapps.week2day1hw_datapersistence.Constants.SHARE_PREF_LOCATION;
 import static com.mobileapps.week2day1hw_datapersistence.Constants.TABLE_NAME;
 import static com.mobileapps.week2day1hw_datapersistence.Constants.USER_NAME;
 import static com.mobileapps.week2day1hw_datapersistence.Constants.USER_NAME_KEY;
@@ -22,6 +26,7 @@ public class UserProfileEntry extends AppCompatActivity {
 
     TextView tvDisplayStatus;
 
+    SharedPreferences sharedPreferences;
     MySQLLiteHelper mySqlLiteHelper;
 
     @Override
@@ -32,6 +37,13 @@ public class UserProfileEntry extends AppCompatActivity {
         tvDisplayStatus = findViewById(R.id.tvDisplayStatus);
         etFirstName = findViewById(R.id.etFirstName);
         etPassword = findViewById(R.id.etLastName);
+
+        sharedPreferences = getSharedPreferences(SHARE_PREF_LOCATION, Context.MODE_PRIVATE);
+
+        mySqlLiteHelper = new MySQLLiteHelper(this,null );
+
+        String message = "Hello " + sharedPreferences.getString(USER_NAME_KEY, "NO NAME");
+        tvDisplayStatus.setText(message);
     }
 
     public void onButtonClick(View view) {
@@ -40,6 +52,7 @@ public class UserProfileEntry extends AppCompatActivity {
                 if (!etFirstName.getText().toString().isEmpty()) {
                     String userName = etFirstName.getText().toString();
                     String password = etPassword.getText().toString();
+                    Log.d("TAG", "onButtonClick: UserName, Password: " + userName + ", " + password);
                     if (mySqlLiteHelper.insertContact(userName, password)){
                         tvDisplayStatus.setText("DATA SAVED");
                     } else {
